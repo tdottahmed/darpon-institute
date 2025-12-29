53d1627172dabac6099922c98e5dbad07b18e5b8
Tanbir <pondit.tanbir@gmail.com>
2025-12-29 10:31:01 +0600

    add: favicon to website and admin panel

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,23 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Bengali:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @routes
-        @viteReactRefresh
-        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+  <!-- Favicon -->
+  <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png" />
+  <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/png" />
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Bengali:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+
+  <!-- Scripts -->
+  @routes
+  @viteReactRefresh
+  @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+  @inertiaHead
+</head>
+
+<body class="font-sans antialiased">
+  @inertia
+</body>
+
 </html>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,12 +6,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'Laravel') }} - Admin Panel</title>
-
+  <title>{{ config('app.name', 'Darpon') }} - Admin Panel</title>
+  <!-- Favicon -->
+  <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png" />
+  <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/png" />
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.bunny.net">
   <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Bengali:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Bengali:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
 
   <!-- Scripts -->
   @vite(['resources/css/app.css'])
