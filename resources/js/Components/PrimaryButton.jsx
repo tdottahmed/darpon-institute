7db216c0da5fe336df858a10cb9bd77280a7cded
Tanbir <pondit.tanbir@gmail.com>
2025-12-29 16:11:26 +0600

    fix: primary button text

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -8,7 +8,7 @@ export default function PrimaryButton({
         <button
             {...props}
             className={
-                `inline-flex items-center rounded-md border border-transparent bg-primary-600 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-primary-700 focus:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 active:bg-primary-800 dark:bg-primary-500 dark:hover:bg-primary-600 dark:focus:bg-primary-600 ${
+                `inline-flex items-center justify-center rounded-md border border-transparent bg-primary-600 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-primary-700 focus:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 active:bg-primary-800 dark:bg-primary-500 dark:hover:bg-primary-600 dark:focus:bg-primary-600 ${
                     disabled && "opacity-25"
                 } ` + className
             }
